cab56713b212be260a13c29eff6a5c09fae53257
Alex <panches2001@gmail.com>
2022-07-08 03:44:38 +0300

    with css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/counter';
+import './styles/App.css';
 
 function App() {
-   const [value, setvalue] = useState('React')
+   const [value, setvalue] = useState('инпут текст')
   
     return (
     <div className = "App">
-      <ClassCounter/>
+      <div className = "post">
+        <div className ="post__content">
+          <strong>JavaScript</strong>
+          <div>
+            Js - это язык программирования
+          </div>
+        </div>
+        <div className='post__btns'>
+          <button>Удалить</button>
+        </div>
+      </div>
     </div>
     );
 }
