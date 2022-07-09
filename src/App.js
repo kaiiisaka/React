6ac7910b74827cbca8768f90195846d42fbccaf3
Alex <panches2001@gmail.com>
2022-07-09 04:02:16 +0300

    work btns and create/remove posts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/counter';
+import PostForm from './components/PostForm';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './components/UI/Button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 import './styles/App.css';
 
 function App() {
-   const [value, setvalue] = useState('инпут текст')
-  
+    const [posts, setPosts] = useState([
+      {id: 1, title: 'Javascript', body: 'Description'},
+      {id: 2, title: 'Javascript 2', body: 'Description'},
+      {id: 3, title: 'Javascript 3', body: 'Description'},
+    ])
+    //получаем пост из дочернего компонента
+    const removePost = (post) => {
+      setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const createPost = (newPost) => {
+      setPosts ([...posts, newPost])
+    }
+
     return (
     <div className = "App">
-      <div className = "post">
-        <div className ="post__content">
-          <strong>JavaScript</strong>
-          <div>
-            Js - это язык программирования
-          </div>
-        </div>
-        <div className='post__btns'>
-          <button>Удалить</button>
-        </div>
-      </div>
+        <PostForm create ={createPost}/>
+        {posts.length
+          ?
+          <PostList remove={removePost} posts={posts} title="Посты про Js"/>
+          :
+          <h1 style={{textAlign: 'center'}}>
+            Посты не найдены!
+            </h1>
+        } 
     </div>
     );
 }
