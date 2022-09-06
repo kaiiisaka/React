1704884c48ed0be324f6a73d0b49a79fbdee5e7d
Alexandr Melnikov <panches2001@gmail.com>
2022-09-07 01:45:56 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -17,71 +17,19 @@ import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./components/hooks/useFetching";
 import {getPageCount, getPagesArray} from "./utils/pages";
 import Pagination from "./components/UI/pagination/Pagination";
+import {BrowserRouter, Route, Routes, Link, Redirect} from "react-router-dom";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
+import Navbar from "./components/UI/Navbar/Navbar";
+import AppRouter from "./components/AppRouter";
 
 function App() {
-    const [posts, setPosts] = useState([])
-
-    const [filter, setFilter] = useState({sort:'', query:''})
-    const [modal, setModal] = useState(false)
-    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-    const [totalPages, setTotalPages] = useState(0)
-    const [limit, setLimit] = useState(10)
-    const [page, setPage] = useState(1)
-
-
-    const [fetchPosts, isPostsLoading, postError] = useFetching( async () => {
-        const response = await PostService.getAll(limit, page);
-        setPosts(response.data)
-        const totalCount = response.headers['x-total-count']
-        setTotalPages(getPageCount(totalCount, limit))
-    })
-
-    useEffect(() => {
-        fetchPosts()
-    }, [page])
-
-    const createPost = (newPost) => {
-      setPosts ([...posts, newPost])
-        setModal(false)
-    }
-
-    const changePage = (page) => {
-        setPage(page)
-    }
-
-    //получаем пост из дочернего компонента
-    const removePost = (post) => {
-        setPosts(posts.filter(p => p.id !== post.id))
-    }
-
     return (
-    <div className = "App">
-        {/*<button onClick={fetchPosts}>get posts</button>*/}
-        <MyButton style = {{marginTop: 30}} onClick={() => setModal(true)}>
-            Создать пост
-        </MyButton>
-        <MyModal visible={modal} setVisible={setModal}>
-            <PostForm create ={createPost}/>
-        </MyModal>
-        <hr style={{margin: '15px 0'}}/>
-        <PostFilter
-            filter={filter}
-            setFilter={setFilter}
-        />
-        {postError &&
-        <h1>произошла ошибка ${postError}</h1>
-        }
-        {isPostsLoading
-        ? <div style={{display: "flex", justifyContent: "center", marginTop: 50}}><Loader/></div>
-        :<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про Js"/>
-        }
-        <Pagination
-            page={page}
-            changePage={changePage}
-            totalPages={totalPages}
-        />
-    </div>
-    );
+        <BrowserRouter>
+                <Navbar/>
+                <AppRouter/>
+        </BrowserRouter>
+    )
 }
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+            <h1>
+                app wrote for myself educating
+            </h1>
+    );
+};
+
+export default About;
