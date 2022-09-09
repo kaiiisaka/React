86f0ffd9e881a16f6bcedc64c4e71ae1f4d75601
Alexandr Melnikov <panches2001@gmail.com>
2022-09-09 22:13:23 +0300

    login

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import About from "../pages/About";
 import Posts from "../pages/Posts";
+import PostIdPage from "../pages/PostIdPage";
+import {routes, publicRoutes} from "../router";
+import Login from "../pages/Login";
 
 const AppRouter = () => {
+
+     const isAuth = true;
+
     return (
-        <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/posts" element={<Posts />} />
-            <Route path="/" element={<Posts />} />
-        </Routes>
+        isAuth === true
+            ?
+            <Routes>
+                <Route path="/about" element={<About />} />
+                <Route path="/posts" element={<Posts />} />
+                <Route path="/" element={<Posts />} />
+                <Route  path="/posts/:id" element={<PostIdPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route
+                    path="*"
+                    element={<Navigate to = "/" replace />}
+                />
+            </Routes>
+            :
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route
+                    path="*"
+                    element={<Navigate to = "/login" replace />}
+                />
+            </Routes>
+
     );
 };
 
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,6 +1,7 @@
 import React, {useState, component} from "react";
 import ReactDOM from 'react-dom';
 import MyButton from "./UI/Button/MyButton";
+import {useNavigate, useParams} from 'react-router-dom'
 
 class editPost extends React.Component{
     constructor(props) { // поменять под себя
@@ -13,6 +14,10 @@ class editPost extends React.Component{
 
 const PostItem = (props) => {
 
+    const navigate = useNavigate()
+    console.log(navigate)
+    const {id} = useParams();
+
     return (
             <div className = "post">
         <div className ="post__content">
@@ -21,6 +26,11 @@ const PostItem = (props) => {
           {props.post.body}
           </div>
         </div>
+                <div className='post__btns'>
+                    <MyButton onClick= {() => navigate(`${props.post.id}`)}>
+                        Открыть
+                    </MyButton>
+                </div>
                 <div className='post__btns'>
                     <MyButton onClick= {() => props.remove(props.post)}>
                         Удалить
